7cb0e5f6d8da5e468635583cf4e7b9ebfb54c8b1
bracha-shreiber <b0583270458@gmail.com>
2025-02-05 01:29:45 +0200

    need add table

--- src/components/user/avatar.tsx
+++ src/components/user/avatar.tsx
@@ -1,17 +1,4 @@
-// import { useContext } from "react";
-// import Avatar from '@mui/material/Avatar';
-// import { userContext } from "../../App";
-// export default () => {
-//     const { user } = useContext(userContext);
-//     const getInitials = (firstName:string, lastName:string) => {
-//         return (firstName.charAt(0) + lastName.charAt(0)).toLowerCase();
-//     };
-//     return (
-//         <Avatar sx={{bgcolor: "red"}}>
-//             {getInitials(user.firstName?user.firstName:'', user.lastName?user.lastName:'')}
-//         </Avatar>
-//     );
-// };
+
 import { useContext } from "react";
 import Avatar from '@mui/material/Avatar';
 import { userContext } from "../../App";
